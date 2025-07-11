e531899bf8be861919cce1a66b85da47d9c6eb50
muya17 <zachariahmuyakapya@gmail.com>
2025-07-11 11:06:44 +0800

    lets see if it works

--- muya/try.c
+++ muya/try.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("hi\n");
+    printf("hi\n. How old are yoy? \n");
+    int a;
+
+    scanf("%d", &a);
+
+    printf("You are %d years old.\n", a);
     return 0;
 }
